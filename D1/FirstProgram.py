705a48bd43e52c816af9a4e1967538cb60a2f9f0
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-12 13:46:07 +0545

    Accessing parts of a string ||  slicing of string

--- D1/FirstProgram.py
+++ D1/FirstProgram.py
@@ -1,13 +1,24 @@
-str1 = "hello  world i am \n jiban pandey \t"
-str2 = "i \t am a full stack web developer"
+str1 = " jiban pandey"
+str2 = "web developer"
 
 # concatenation
 print(str1 + str2) 
 # length
- 
-
 print(len(str1 + str2 + " "))
 # indexing of string in python
 print(str1[0])
 print(str2[9])
 # we acces the index of character in string but we can not change it
+
+            #slici
+# //  Accessing parts of a string
+# sentex => str[stating_index : ending_index]
+
+print(str2[0 : 14])
+print(str2[4 : ])
+print(str2[: 4])
+print(str2[ : ])
+# backward counter
+print(str2[ :-1])
+print(str2[ :-9])
+print(len(str2[ :-9]))

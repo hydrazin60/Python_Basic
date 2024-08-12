bfb2936ee223a48f9164cd53c180a3ed0a4fb54b
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-12 13:30:20 +0545

    string length , concatenation , number of words in the string in JS

--- D1/FirstProgram.py
+++ D1/FirstProgram.py
@@ -1,1 +1,12 @@
-print("Hello world")
+str1 = " hello world i am \n jiban pandey \t"
+str2 = "i \t am a full stack web developer"
+
+# concatenation
+print(str1 + str2) 
+
+# length
+print(len(str1 + str2 + " "))
+
+# slicing
+ 
+

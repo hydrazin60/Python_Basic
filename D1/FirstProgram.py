093febc4569de7bf435c4dfde37284c976eeac5e
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-12 13:36:17 +0545

    indexing of string in python

--- D1/FirstProgram.py
+++ D1/FirstProgram.py
@@ -1,12 +1,13 @@
-str1 = " hello world i am \n jiban pandey \t"
+str1 = "hello  world i am \n jiban pandey \t"
 str2 = "i \t am a full stack web developer"
 
 # concatenation
 print(str1 + str2) 
-
 # length
-print(len(str1 + str2 + " "))
-
-# slicing
  
 
+print(len(str1 + str2 + " "))
+# indexing of string in python
+print(str1[0])
+print(str2[9])
+# we acces the index of character in string but we can not change it
--- D1/basic.js
+++ D1/basic.js
@@ -2,7 +2,7 @@ let str1 =
   "hello world \n jdn dfh b bcxnxnc jiban nxcxc xbcx cnak b cnx xn zmxbcxcb  improve query performance, especially if you frequently search or sort by these fields.improve query performance, especially if you frequently search or sort by these fields These changes enhance the schema by ensuring data integrity, improving performance, and making it easier to work with comments in your application ";
 console.log(str1);
 // length of the string
- console.log(str1.length);
- 
+console.log(str1.length);
+
 //  number of words in the string
 console.log(str1.split(" ").length);

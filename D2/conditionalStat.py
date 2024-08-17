96de7504b603d0e3e313f83601064ab1859497b5
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-17 19:01:46 +0545

    is elif statement

--- D2/conditionalStat.py
+++ D2/conditionalStat.py
@@ -1,22 +1,29 @@
-#  CONDITIONAL STATEMENTS
+### CONDITIONAL STATEMENTS
 
-# 1 if statement
-# 2 if-else statement
-# 3 if-elif-else statement
-# 4 nested if statement
-# 5 ternary operator
+### 1 if statement
+### 2 if-else statement
+### 3 if-elif-else statement
+### 4 nested if statement
+### 5 ternary operator
 
 ## if statement
-# if(condition) : statement1
-# else : statement2
+## if(condition) : statement1
+## else : statement2
 
-age = int(input("Enter your age: "))  
-if 18 <= age < 60:
-    print("Eligible for driving license")
-elif age < 18:
-    print("Not eligible for driving license")
-elif age >= 60:
-    print("Senior citizen eligible for driving license")
-else:
-    print("Not eligible")
+#### Example:1
+# age = int(input("Enter your age: "))  
+# if 18 <= age < 60:
+#     print("Eligible for driving license")
+# elif age < 18:
+#     print("Not eligible for driving license")
+# elif age >= 60:
+#     print("Senior citizen eligible for driving license")
+# else:
+#     print("Not eligible")
 
+#### Example:2
+lightColor = "yellow"
+if(lightColor == "red"): print("stop")
+elif(lightColor == "yellow"): print("slow")
+elif(lightColor == "green"): print("go")
+else: print("invalid color")

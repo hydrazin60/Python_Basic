685632ecfe603abc31c322cf06739cde95020da7
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-18 18:45:23 +0545

    Lists in Python

---
+++ D3/List&Tuples.py
@@ -0,0 +1,21 @@
+#### Lists in Python
+## string => inmutable in python(unchangeable)  [in string we can read and access the index(value) BUT we can not change the value]
+## list => mutable in python (changeable) [in list we can read and access the index(value) AND we can change the value]
+marks1 = 95.3
+marks2 = 85.6
+marks3 = 75.9
+marks4 = 65.4
+marks5 = 55.9
+marks6 = 45.2
+
+marks = [95.3, 85.6, 75.9, 65.4, 55.9, 45.2]
+studentNams = ["jiban", "pandey", "khem", "karuna", "nisan", "samir"]
+# print(studentNams[0], ":",  marks[2])
+## we can change the value i List
+# marks[0] = 100
+# studentNams[1] = "Pralhad giri"
+# print(studentNams[1] , marks[0]) 
+### slicing of list @@  List_name[starting index : ending index]  @@ List_name[starting index : ending index : step size]
+# print(marks[0:3]) ## from 0 to 2
+# print(marks[:4]) ## from 0 to 5  @@start:stop
+ 

e0fd3408bb196275858989e1741b9de601dc2fac
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-20 18:45:23 +0545

    nested Dictionary in Python

--- D4/Dictionary&set.py
+++ D4/Dictionary&set.py
@@ -7,11 +7,23 @@ students = {
     "subject" : ["python", "javascript", "C++"],
     "favrit" : ("Javascript" , "python"),
     "pandey": 85.6, 
-    "khem": 75.9, 
+    "khem": {
+"first-name": "karuna",
+"second-name": "karki",
+"Age" : 20,
+"marks" : [95.3, 85.6, 75.9, 65.4, 55.9, 45.2],
+"location": {
+    "permanant": "sindhuli",
+    "current" : "dharan"
+}
+
+    },
     "karuna": 65.4, 
     "nisan": True, 
     "samir":  "giri"
 }
-
 print(students)
+print(students["jiban"]) ## accessing the value using key
+students["jiban"] = 100 ## updating the value using key
+print(students["jiban"])
  

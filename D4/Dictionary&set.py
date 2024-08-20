14a6ecd946c355965205c530f3894c228466a9e7
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-20 18:33:49 +0545

    Dictionary in Python

---
+++ D4/Dictionary&set.py
@@ -0,0 +1,17 @@
+######  Dictionary in Python ####### ( similar to object in javascript) 
+### :- Dictionaries are used to store data values in key:value pairs
+### :-  They are unordered, mutable(changeable) & don’t allow duplicate keys
+
+students = {
+    "jiban": 95.3,
+    "subject" : ["python", "javascript", "C++"],
+    "favrit" : ("Javascript" , "python"),
+    "pandey": 85.6, 
+    "khem": 75.9, 
+    "karuna": 65.4, 
+    "nisan": 55.9, 
+    "samir": 45.2
+}
+
+print(students)
+ 

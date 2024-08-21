196f82e65ce6b327391bba75099458b13a31badb
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-21 17:43:46 +0545

    while loop basic  3question

--- D4/Dictionary&set.py
+++ D4/Dictionary&set.py
@@ -22,8 +22,47 @@ students = {
     "nisan": True, 
     "samir":  "giri"
 }
-print(students)
-print(students["jiban"]) ## accessing the value using key
-students["jiban"] = 100 ## updating the value using key
-print(students["jiban"])
+# print(students)
+# print(students["jiban"]) ## accessing the value using key
+# students["jiban"] = 100 ## updating the value using key
+# print(students["jiban"])
  
+######### Dictionary Methods  ##############
+## Dictionary.keys()  # return all the keys
+# print(students.keys())  # return all the keys
+# print(len(students.keys())) # return the number of keys
+## Dictionary.values()  # return all the values
+#print(students.values()) # return all the key and values 
+## Dictionary.items()  # return all the keys and values
+# print(students.items()) # return all the key and values
+## Dictionary.get(key)  # return the value of the key 
+# print(students.get("jiban")) # return the value of the induvisial key
+# print(students["jiban"]) #  return the value of the induvisial key
+## Dictionary.update()  # update the dictionary
+# students.update({"jiban": 100}) # update the value of the key
+# print(students)
+ 
+########## Set in Python  ###########
+### : Set is the collection of the unordered items.
+### : Each element in the set must be unique & immutable.
+
+set1 = {"jiban", "pandey",   100, 200, 300,   "samir", "pandey" }
+set2 = {"jiban" , "pandey" , 200 , "karuna"}
+# # print(set1)
+# #########  Set Methods  ############
+# ## set.add( el )
+# set1.add("bipana") # add the element ## bipana is add in the set
+# # print(set1)
+# ## set.remove( el )
+# set1.remove( "bipana") # remove the element
+# # print(set1)
+# ## set.clear( )
+# # set1.clear() # clear the set
+# # print(set1)
+# ## set.pop( )
+# set1.pop() # remove the last element
+# # print(set1)
+# ## set.union( sset2) # return the union of two sets
+# # print(set1.union(set2))   # return the union of two sets but is not change the orginal set
+# ## set.intersection( sset2) # return the intersection of two sets but is not change the orginal set
+# print(set1.intersection(set2))
---
+++ D5/Loops.py
@@ -0,0 +1,38 @@
+###### Loops in Python ########
+### while loop
+### for loop
+
+## while loop
+# count = 0
+# while count < 5:
+#  print("hello", count)
+#  count = count + 1
+
+## Q1) Print numbers from 1 to 100
+# count =1 
+# while count<=100:
+#     print(count)
+#     count = count + 1
+
+## Q2) Print the multiples table of 'n' 
+# n = int(input("Enter the number : "))
+# count = 1
+# while count <= 10:
+#     print(n ,"*", count ,"=",n*count)
+#     count = count + 1 
+
+## Q3) Print the numbers from 1 to 100 which are divisible by 'n'
+# n = int(input("Enter the number : "))
+# count = 1
+# while count <= 100:
+#    count = count + 1
+#    if(count % n == 0):
+#        print(count)
+## Q4) Print the elements of the following list using a loop:
+##[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# n = 1
+# while n <= 10:
+#     print(n*n)
+#     n = n + 1
+
+## Q5)   Search for a number x in this tuple using loop: [1, 4, 9, 16, 25, 36, 49, 64, 81,100]

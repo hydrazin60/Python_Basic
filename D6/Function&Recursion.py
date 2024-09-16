64a928e02151a42ad0f50161cc000e141ab73051
hydrazin60 <sureshkhdk45@gmail.com>
2024-09-16 16:25:55 +0545

    some question of function in python

--- D6/Function&Recursion.py
+++ D6/Function&Recursion.py
@@ -37,3 +37,29 @@
 ## in phthon have 2 types of function 
 ## i) Built-in Function :- print() , len() , type() , range() ....
 ## ii) User-defined Function
+
+## EX3
+# def cal_Product (a,b=1): ## we can define default value and default value is placed at the end not start
+#     product = a*b
+#     return product
+
+# print(cal_Product(5,6))
+
+################### LET's Practice ################
+##Q1) WAF to print the length of a list.(list is the parameter)
+
+cities = ["kathmandu", "lalitpur", "bhaktapur", "pokhara", "chitwane"]
+heros = ["ironMan", "hulk", "thor", "spiderman", "captainAmerica"]
+
+# def print_len(list):
+#     print(len(list))
+ 
+# print_len(cities)
+# print_len(heros)
+
+# Q2) waf to print the elements of a list in a single line.(list is the parameter)
+# def print_List(list):
+#  for item in list:
+#      print(item, end=" ")
+
+# print_List(cities)

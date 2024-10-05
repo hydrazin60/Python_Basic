bd530d9ba8b81f5331b6605f98a99d542e89479b
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-05 18:48:06 +0545

    # slicing of string

--- CodeHarry/01_intro_strings.py
+++ CodeHarry/01_intro_strings.py
@@ -1,1 +1,21 @@
+name = "Jiban"
+print(name[2:4]) # ans => ba
+
+##  the index in a string starts from 0 to length - 1 in python . in order 
+##  to slice a string , we used the following syntex
+##  newvariable =  string_name[starting index : ending index]
+##  {in python string are immutable}
+
+
+String = "jiban"
+## position of index of string is we can write zero '0' to last index  AND
+## also we can write -1 to last index
+## in example   j => 0,-5
+##              i => 1,-4
+##              b => 2,-3
+##              a => 3,-2
+##              n => 4,-1
+print(String[-4:4])  # ans => iba
+
+
  

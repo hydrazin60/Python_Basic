b6c8b4f133c12462c9386ff2c372150fd2d8c422
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-05 19:00:58 +0545

    SLICING WITH SKIP VALUE

--- CodeHarry/01_intro_strings.py
+++ CodeHarry/01_intro_strings.py
@@ -1,21 +1,27 @@
-name = "Jiban"
-print(name[2:4]) # ans => ba
+# name = "Jiban"
+# print(name[2:4]) # ans => ba
 
-##  the index in a string starts from 0 to length - 1 in python . in order 
-##  to slice a string , we used the following syntex
-##  newvariable =  string_name[starting index : ending index]
-##  {in python string are immutable}
+# ##  the index in a string starts from 0 to length - 1 in python . in order 
+# ##  to slice a string , we used the following syntex
+# ##  newvariable =  string_name[starting index : ending index]
+# ##  {in python string are immutable}
 
 
-String = "jiban"
-## position of index of string is we can write zero '0' to last index  AND
-## also we can write -1 to last index
-## in example   j => 0,-5
-##              i => 1,-4
-##              b => 2,-3
-##              a => 3,-2
-##              n => 4,-1
-print(String[-4:4])  # ans => iba
+# String = "jiban"
+# ## position of index of string is we can write zero '0' to last index  AND
+# ## also we can write -1 to last index
+# ## in example   j => 0,-5
+# ##              i => 1,-4
+# ##              b => 2,-3
+# ##              a => 3,-2
+# ##              n => 4,-1
+# print(String[-4:4])  # ans => iba
+ 
 
+######### SLICING WITH SKIP VALUE ########
+
+## we can provide a skip value as a part of our slice like this :
+name = "Jibanpandey"
+print(name[0:8 : 3]) # ANS => Jaa
 
  
